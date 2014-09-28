d03688c253855215342e46b1855cb4f6dee05887
Tim Hamer <tim@arcaexterna.com>
2014-09-28 16:01:32 -0500

    app with two buttons. does nothing yet. widget isn't working yet

---
+++ app/src/main/java/com/arcaexterna/windowremote/WidgetConfigurationActivity.java
@@ -0,0 +1,22 @@
+package com.arcaexterna.windowremote;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by tim on 9/28/14.
+ */
+public class WidgetConfigurationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.widget_config);
+    }
+
+    public void okButtonPressed(View view) {
+
+    }
+
+}
---
+++ app/src/main/java/com/arcaexterna/windowremote/WindowRemoteWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.arcaexterna.windowremote;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by tim on 9/28/14.
+ */
+public class WindowRemoteWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
